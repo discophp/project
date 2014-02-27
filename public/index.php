346ec657959a260b9b5574bc9c4b71af8858d357
root <root@mint>
2014-02-27 11:17:57 -0500

    updated pathing to Core Disco File

--- public/index.php
+++ public/index.php
@@ -7,16 +7,16 @@ $time = microtime();
  * Load Disco 
  * The only require you must write
 */
-require_once('../core/Disco.core.php');
+require_once('../vendor/jbhamilton/disco-core/core/Disco.core.php');
 
 
 
 /**
  * Swap the View Facade with a new instance 
 */
-Disco::make('View',function(){
-    return new bitcoin();
-});
+//Disco::make('View',function(){
+//    return new bitcoin();
+//});
 
 
 
