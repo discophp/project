d0dccb82ca61dcfd4ccc784a52909e610a9082b8
root <root@mint>
2014-02-27 10:35:49 -0500

    strapped

---
+++ public/index.php
@@ -0,0 +1,46 @@
+<?php
+
+//debug
+$time = microtime();
+
+/**
+ * Load Disco 
+ * The only require you must write
+*/
+require_once('../core/Disco.core.php');
+
+
+
+/**
+ * Swap the View Facade with a new instance 
+*/
+Disco::make('View',function(){
+    return new bitcoin();
+});
+
+
+
+
+/**
+ * Set up a Router
+*/
+//Disco::useRouter('bitcoin/bitcoinRouter');
+
+
+
+
+/**
+ * Print out our View
+*/
+View::printPage();
+
+
+
+
+
+
+
+//debug
+echo '<script>console.log("'.(microtime()-$time).'");</script>';
+
+?>
