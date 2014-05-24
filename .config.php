4d9d7ae212c40f2722dd951374d3a81993add519
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-05-24 17:21:33 -0400

    moved from using json based setting files to php configuration files

---
+++ .config.php
@@ -0,0 +1,28 @@
+<?php
+
+return Array(
+
+    'URL'=>'localhost/',
+
+    'COMPOSER_PATH'=>'vendor',
+
+    'APP_MODE'=>'DEV',
+
+    'DB_HOST'=>'localhost',
+    'DB_USER'=>'root',
+    'DB_PASSWORD'=>'password',
+    'DB_DB'=>'testdb',
+
+
+    'MEMCACHE_HOST'=>'localhost',
+    'MEMCACHE_PORT'=>'11211',
+
+
+    'AES_KEY256'=>'You must set this to a 32 length string for secure encryption',
+
+
+    'SHA512_SALT_LEAD'=>'somerandomstringhash',
+    'SHA512_SALT_TAIL'=>'morerandom'
+);
+
+?>
---
+++ .mail.config.php
@@ -0,0 +1,23 @@
+<?php
+return Array(
+        'DEFAULT'=>'SMTP',
+        'DEV_MODE_SEND_TO'=>'sendtestemails@testing.com',
+        'johns-email'=>Array(
+            'NAME'=>'John Smith',
+            'EMAIL'=>'john@gmail.com',
+            'PASSWORD'=>'johnspassword'
+        ),
+        'SMTP'=>Array(
+            'HOST'=>'mail.messagingengine.com',
+            'PORT'=>'465'
+        ),
+        'SSL'=>Array(
+            'HOST'=>'mail.messagingengine.com',
+            'PORT'=>'993'
+        ),
+        'TLS'=>Array(
+            'HOST'=>'mail.site.com',
+            'PORT'=>'465'
+        )
+);
+?>
