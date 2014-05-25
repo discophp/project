f7498a37674fe08671924e50744b823c2d774355
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-05-25 10:43:42 -0400

    cleaned up index file and re-added namespace to shipped Util Facade

---
+++ app/facade/Util.facade.php
@@ -0,0 +1,12 @@
+<?php
+
+class Util extends Disco\classes\Facade {
+
+    protected static function returnFacadeId(){
+        return 'Util';
+    }//returnFacadeId
+
+}//Facade
+
+
+?>
--- public/index.php
+++ public/index.php
@@ -7,33 +7,39 @@
 require_once('../vendor/discophp/framework/core/Disco.core.php');
 
 
+
+/**
+ *
+ *              YOUR APPLICATION LOGIC GOES BELOW
+ *              ---------------------------------
+ *
+*/
+
+
+
+
+
 /**
  *      Don't want to use the Util Facade? Comment below out,or delete it.
+ *        - you can subsequently remove the Util.class.php file from app/class/
 */
 
 Disco::make('Util',function(){
-    return new Disco/classes/Util;
+    return new Disco\classes\Util;
 });
 
 
-
 /**
- *
- *      YOUR APPLICATION LOGIC GOES BELOW
- *      ---------------------------------
- *
-*/
-
-
-
-/**
- * Swap the View Facade with a different Base object
+ *      Swap the View Facade With an Extended View Class 
 */
 Disco::make('View',function(){
     return new StandardView();
 });
 
 
+
+
+//match the index route
 Router::get('/',function(){
 
     View::html('Disco is working');
@@ -45,13 +51,38 @@ Router::get('/',function(){
 
 
 
+/**
+ *
+ *              ---------------------------------
+ *              YOUR APPLICATION LOGIC STAYS ABOVE 
+ *
+*/
 
 
 
 /**
- * Print out our View
- * Do no remove this method call unless you wish to 
- * call View::printPage() after satisifying every router condition
+ *      ---------- 404 --------------
+*/
+Router::get('/404',function(){
+    header('HTTP/1.0 404 Not Found');
+    View::html('<h1>404</h1>');
+});
+
+
+/**
+ *      If we never resolve a Router to an endpoint then we need to send
+ *      the user to the 404 page
+ */
+if(!Router::routeMatch()){
+    header('Location:/404');
+    exit;
+}//if
+
+
+/**
+ *      Print out our View
+ *      Do no remove this method call unless you wish to 
+ *      call View::printPage() after satisifying every router condition
 */
 View::printPage();
 
