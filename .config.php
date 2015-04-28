3bf60843564ca6b829a21b93bdcf99d77eaf81b2
jbhamilton <brad@bradleyhamilton.com>
2015-04-28 16:28:32 -0400

    updating to 2.3

--- .config.php
+++ .config.php
@@ -2,9 +2,11 @@
 
 return Array(
 
-    'URL'=>'localhost/',
+    'URL'=>'localhost',
 
     'COMPOSER_PATH'=>'vendor',
+
+    'TEMPLATE_EXTENSION'=>'',
 
     'APP_MODE'=>'DEV',
     'MAINTENANCE_MODE'=>'NO',
@@ -19,11 +21,11 @@ return Array(
     'MEMCACHE_PORT'=>'11211',
 
 
-    'AES_KEY256'=>'You must set this to a 32 length string for secure encryption',
+    'AES_KEY256'=>'8348515285f6d1e9826c5dd53ec8c404',
 
 
-    'SHA512_SALT_LEAD'=>'somerandomstringhash',
-    'SHA512_SALT_TAIL'=>'morerandom'
+    'SHA512_SALT_LEAD'=>'18109d6d31ba',
+    'SHA512_SALT_TAIL'=>'4ec0bf16dc2f'
 );
 
 ?>
