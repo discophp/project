aea38238a33ffb59c8be0a0c89a7b59fb64fbad2
jbhamilton <brad@bradleyhamilton.com>
2016-02-08 16:31:16 -0500

    travis

--- app/config/config.php
+++ app/config/config.php
@@ -15,10 +15,10 @@ return Array(
     'DB_ENGINE'             => 'mysql',
     'DB_CHARSET'            => 'utf8',
 
-    'DB_HOST'               => '',
-    'DB_USER'               => '',
+    'DB_HOST'               => '127.0.0.1',
+    'DB_USER'               => 'root',
     'DB_PASSWORD'           => '',
-    'DB_DB'                 => '',
+    'DB_DB'                 => 'YOUR_DB',
 
     'AES_KEY256'            => '',
 
